b89a8bc0cb2859576849562f24e43821eca8ec35
Rifa <rifa.aulia@mhs.itenas.ac.id>
2023-03-20 11:18:37 +0700

    berka jelek

---
+++ Nyoba.php
@@ -0,0 +1,12 @@
+<?php
+    $a="12";
+    $b=28;
+    $hasil=$a + $b;
+
+    $cetak = print"hasil = $hasil <br>";
+
+    echo("$cetak <br>");
+    echo "Hello World! <br>";
+    printf("Hello World 2! <br>");
+
+?>
